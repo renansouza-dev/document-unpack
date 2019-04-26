b9952eea82a7d1cd9ac1d00569fbdd1a5c1f5b6e
rsouza <renan.souza@neogrid.com>
2019-04-26 13:37:38 -0300

    Fix unused imports and zip text file. Add a git ignore file.

--- src/main/java/dev/renansouza/document/DocumentController.java
+++ src/main/java/dev/renansouza/document/DocumentController.java
@@ -1,41 +1,39 @@
 package dev.renansouza.document;
 
+import java.io.File;
+
+import io.micronaut.core.util.StringUtils;
 import io.micronaut.http.HttpResponse;
+import io.micronaut.http.HttpStatus;
 import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
-import io.micronaut.http.HttpStatus;
 import io.micronaut.http.annotation.Post;
 import io.micronaut.http.multipart.StreamingFileUpload;
 import io.reactivex.Single;
-import io.reactivex.disposables.Disposable;
-import org.reactivestreams.Publisher;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import javax.inject.Inject;
-import java.io.File;
-import java.io.IOException;
 
 @Controller("/invoice")
 public class DocumentController {
 
     private static final Logger log = LoggerFactory.getLogger(DocumentController.class);
-    private String exception = "";
 
-    @Get("/")
+    @Get()
     public HttpStatus index() {
         return HttpStatus.OK;
     }
 
     //TODO Alter environment and flow to ENUM
     @Post(value = "/unpack", consumes = MediaType.MULTIPART_FORM_DATA)
-    public Single<HttpResponse<String>> upload(StreamingFileUpload file, int environment, int flow) throws IOException {
+    public Single<HttpResponse<String>> upload(StreamingFileUpload file, int environment, int flow) {
+        final StringBuilder exception = new StringBuilder();
+
         return Single.just(new Document(file.getFilename(), environment, flow))
             .flatMap(DocumentValidation::validateDocumentExtension)
             .doOnError(throwable -> {
                 log.error("Validation exception: {}", throwable.getMessage());
-                exception = throwable.getMessage();
+                exception.append(throwable.getMessage());
             })
             .doOnSuccess(doc -> {
                 log.info("File saved successfuly");
@@ -43,10 +41,10 @@ public class DocumentController {
                 file.transferTo(tempFile);
             })
             .map(success -> {
-                if (exception != null || !exception.equals("")) {
+                if (!StringUtils.hasText(exception.toString())) {
                     return HttpResponse.<String>status(HttpStatus.CREATED).body("Uploaded");
                 } else {
-                    return HttpResponse.<String>status(HttpStatus.CONFLICT).body(exception);
+                    return HttpResponse.<String>status(HttpStatus.CONFLICT).body(exception.toString());
                 }
             });
     }
